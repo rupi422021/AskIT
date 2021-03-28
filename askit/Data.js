2c6adc4de88f7b1a7de51d22fb4e1891ae9ae9dc
rupi422021 <77380557+rupi422021@users.noreply.github.com>
2021-03-28 20:41:18 +0300

    First Upload

---
+++ askit/Data.js
@@ -0,0 +1,65 @@
+﻿Institutions = [
+    {
+        "רופין": [{
+            "statusInstitute": "Active",
+            "Departments": [{
+                "הנדסה תעשייה וניהול":{
+                    "statusDepartmente": "Active",
+                    "Courses": [{
+                        "SQL":{
+                            "statusCourse": "Active",
+                            "Subjects": [{
+                                "ERD":{
+                                    "statusSubject": "Active",
+                                    "Questions": [{
+                                        "רובוטריקים1":{
+                                            "type": "Open",
+                                            "content": "bla bla bla",
+                                            "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F614459942880927462%2F&psig=AOvVaw31tG7Fch359KyNnd2qJm1F&ust=1615453939178000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJimi4Wxpe8CFQAAAAAdAAAAABAD",
+                                            "creationDate": "09/03/2021",
+                                            "tags": ["Select", "update", "triger"],
+                                            "level": 3,//hard,medium,easy
+                                            "publish": True,//True=yes,False=no
+                                            "wherepublish": homework,
+                                            "publishdate": "09/03/2021",
+                                            "access": True //True=open,False=Close
+                                        },
+                                        "רובוטריקים2": {
+                                            "type": "Open",
+                                            "content": "bla bla bla",
+                                            "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F614459942880927462%2F&psig=AOvVaw31tG7Fch359KyNnd2qJm1F&ust=1615453939178000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJimi4Wxpe8CFQAAAAAdAAAAABAD",
+                                            "creationDate": "09/03/2021",
+                                            "tags": ["Select", "update", "triger"],
+                                            "level": 3,//hard,medium,easy
+                                            "publish": True,//True=yes,False=no
+                                            "wherepublish": homework,
+                                            "publishdate": "09/03/2021",
+                                            "access": True //True=open,False=Close
+                                        }
+                                    }]
+                                },
+                                "Queries":{
+                                    "statusSubject": "Active",
+                                    "Questions": [{
+                                        "ספיידרמן1":{
+                                            "type": "Open",
+                                            "content": "הצג את כל הספיידרמנים בכחול",
+                                            "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F614459942880927462%2F&psig=AOvVaw31tG7Fch359KyNnd2qJm1F&ust=1615453939178000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJimi4Wxpe8CFQAAAAAdAAAAABAD",
+                                            "creationDate": "10/03/2021",
+                                            "tags": ["Select", "update", "orderby"],
+                                            "level": 1,//hard,medium,easy
+                                            "publish": True,//True=yes,False=no
+                                            "wherepublish": homework,
+                                            "publishdate": "09/03/2021",
+                                            "access": True //True=open,False=Close
+                                        }
+                                    }]
+                                }
+                            }]
+                        }
+                    }]
+                }
+            }]
+        }]
+    }];
+             
---
+++ askit/config.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBL_liEzfsMvIf3xvPNloLkT2W1hfNFmHA",
+    authDomain: "askit-35d7a.firebaseapp.com",
+    databaseURL: "https://askit-35d7a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "askit-35d7a",
+    storageBucket: "askit-35d7a.appspot.com",
+    messagingSenderId: "630976143083",
+    appId: "1:630976143083:web:7b5f9a1d6880a8767328ae",
+    measurementId: "G-P1DYTTGTCD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
